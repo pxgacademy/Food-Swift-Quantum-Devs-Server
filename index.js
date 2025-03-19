1e68619064457a8f697a16027dba1945c29f7244
Mahdi Hassan <mahdioffi333@gmail.com>
2025-03-19 23:45:36 +0600

    added socket.io basic setup

--- index.js
+++ index.js
@@ -4,20 +4,37 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const http = require("http");
+const { Server } = require("socket.io");
 
 const app = express();
 const port = process.env.PORT || 5000;
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://food-delivery-app-quantum-devs.web.app/",
+    ],
+    credentials: true,
+  },
+});
 
 // Middlewares
 app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://food-delivery-app-quantum-devs.web.app/",
+    ],
     credentials: true,
   })
 );
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uioun.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // const uri = "mongodb://localhost:27017";
 
@@ -49,6 +66,20 @@ async function run() {
     const db = client.db("Personal_Portfolio");
     // create your collection here
     // const skillCollection = db.collection("skills");
+
+    // socket.io
+    io.on("connection", (socket) => {
+      console.log("socket.io connected", socket.id);
+
+      socket.on("message", (data) => {
+        console.log("message received", data);
+        io.emit("message", data);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("socket.io disconnected", socket.id);
+      });
+    });
 
     const cookieOptions = {
       httpOnly: true,
@@ -99,6 +130,6 @@ app.use((err, req, res, next) => {
     .send({ message: "Internal Server Error", error: err.message });
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
